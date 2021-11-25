a017feee5a90f5c884b1161cf2d372ebb0094568
Bartosz Malecki <bartoszmalecki90@gmail.com>
2021-11-25 14:40:06 +0100

    events trying to do

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,36 @@
-import '../scss/main.scss';
+import "../scss/main.scss";
 
-// uncomment the lines below to enable PWA
-// import {registerSW} from './pwa.js';
-// registerSW();
+const daily = document.querySelector(".daily--js");
+const weekly = document.querySelector(".weekly--js");
+const monthly = document.querySelector(".monthly--js");
+const activDaily = document.querySelectorAll(".daily");
+const activWeekly = document.querySelectorAll(".weekly");
+const activMonthly = document.querySelectorAll(".monthly");
 
-/* place your code below */
+// daily.addEventListener("click", function () {});
 
-console.log('HELLO 🚀')
+const showDaily = function () {
+  //   activWeekly.classList.add("hidden");
+  //   activMonthly.classList.add("hidden");
+  activDaily.classList.remove("hidden");
+  console.log("as");
+};
+
+const showWeekly = function () {
+  activDaily.classList.add("hidden");
+  activMonthly.classList.add("hidden");
+  activWeekly.classList.remove("hidden");
+  console.log("dupa");
+};
+
+const showMonthly = function () {
+  activWeekly.classList.add("hidden");
+  activDaily.classList.add("hidden");
+  activMonthly.classList.remove("hidden");
+  console.log("elo");
+};
+
+daily.addEventListener("click", showDaily);
+
+activWeekly.forEach((p) => p.addEventListener("click", showWeekly));
+activMonthly.forEach((p) => p.addEventListener("click", showMonthly));
